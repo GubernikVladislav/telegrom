007ac99637bd47a27e1019f6fef2c20fc857a0c1
Губерник Владислав Игоревич <Gubernik.V.Ig@sberbank.ru>
2021-03-14 12:42:33 +0300

    init commit

---
+++ src/main/java/ru/cool/telegrom/TelegromApplication.java
@@ -0,0 +1,13 @@
+package ru.cool.telegrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegromApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegromApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/cool/telegrom/TelegromApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cool.telegrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegromApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
